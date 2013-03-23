6865e78e1915a201bca7d3485815fdb90d06c1ff
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-23 16:03:15 +0400

    add reverse mode!

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var through = require('pull-through')
-
-module.exports = function split (matcher, mapper) {
+function split (matcher, mapper, reverse) {
   var soFar = ''
   if('function' === typeof matcher)
     mapper = matcher, matcher = null
@@ -9,11 +8,16 @@ module.exports = function split (matcher, mapper) {
 
   return through(function (buffer) { 
     var stream = this
-      , pieces = (soFar + buffer).split(matcher)
-    soFar = pieces.pop()
+      , pieces = (
+      reverse ? (buffer + soFar) : (soFar + buffer)
+    ).split(matcher)
+    soFar = reverse ? pieces.shift() : pieces.pop()
 
+    /*
+      var max = pieces.length - 1
+    
     for (var i = 0; i < pieces.length; i++) {
-      var piece = pieces[i]
+      var piece = pieces[reverse ? max - i : i]
       if(mapper) {
         piece = mapper(piece)
         if('undefined' !== typeof piece)
@@ -22,6 +26,48 @@ module.exports = function split (matcher, mapper) {
       else
         stream.queue(piece)
     }
+    */
+    while(pieces.length)
+      stream.queue(reverse ? pieces.pop() : pieces.shift())
+  },
+  function () {
+    if(soFar != null)
+      this.queue(soFar)  
+    this.queue(null)
+  })
+}
+
+module.exports = function split (matcher, mapper, reverse) {
+  var soFar = ''
+  if('function' === typeof matcher)
+    mapper = matcher, matcher = null
+  if (!matcher)
+    matcher = '\n'
+
+  return through(function (buffer) { 
+    var stream = this
+      , pieces = ( reverse 
+        ? buffer + soFar
+        : soFar + buffer
+      ).split(matcher)
+
+    soFar = reverse ? pieces.shift() : pieces.pop()
+
+    var l = pieces.length
+    for (var i = 0; i < l; i++) {
+      var piece = pieces[reverse ? l - 1 - i : i ]
+      if(mapper) {
+        piece = mapper(piece)
+        if('undefined' !== typeof piece)
+          stream.queue(piece)
+      }
+      else
+        stream.queue(piece)
+    }
+    //while(pieces.length)
+      //stream.queue(reverse ? pieces.pop() : pieces.shift())
+
+
   },
   function () {
     if(soFar != null)
