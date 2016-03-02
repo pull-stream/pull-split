09d3ab727d3916396675d2c1e54e843b1fbf355f
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-02 20:42:43 +1300

    remember to map the last value

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var through = require('pull-through')
 
-module.exports = function split (matcher, mapper, reverse) {
+module.exports = function split (matcher, mapper, reverse, last) {
   var soFar = ''
   if('function' === typeof matcher)
     mapper = matcher, matcher = null
   if (!matcher)
     matcher = '\n'
+
+  function map(stream, piece) {
+    if(mapper) {
+      piece = mapper(piece)
+      if('undefined' !== typeof piece)
+        stream.queue(piece)
+    }
+    else
+      stream.queue(piece)
+  }
 
   return through(function (buffer) {
     var stream = this
@@ -15,23 +25,21 @@ module.exports = function split (matcher, mapper, reverse) {
       ).split(matcher)
 
     soFar = reverse ? pieces.shift() : pieces.pop()
-
     var l = pieces.length
     for (var i = 0; i < l; i++) {
-      var piece = pieces[reverse ? l - 1 - i : i ]
-      if(mapper) {
-        piece = mapper(piece)
-        if('undefined' !== typeof piece)
-          stream.queue(piece)
-      }
-      else
-        stream.queue(piece)
+      map(stream, pieces[reverse ? l - 1 - i : i ])
     }
   },
   function () {
-    if(soFar != null)
-      this.queue(soFar)
+    if(last && soFar == '')
+      return this.queue(null)
+    else (soFar != null) // && (last && soFar != ''))
+      map(this, soFar)
+
     this.queue(null)
   })
 }
+
+
+
 
